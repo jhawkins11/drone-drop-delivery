aca89783854820c39d38757e7930a93127887f94
jhawkins11 <josiah.c.hawkins@gmail.com>
2023-02-02 14:52:26 -0600

    allow http requests

--- src/helperFunctions/addMarker.ts
+++ src/helperFunctions/addMarker.ts
@@ -1,10 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 const addMarker = async (marker) => {
   try {
+    // const response = await axios.post(
+    //   `${process.env.REACT_APP_API_URL}api/markers`,
+    //   marker
+    // );
+    // rewrite with headers
     const response = await axios.post(
       `${process.env.REACT_APP_API_URL}api/markers`,
-      marker
+      marker,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Security-Policy': 'upgrade-insecure-requests',
+        },
+      }
     );
     if (response.status === 200) {
       return response.data;
--- src/helperFunctions/fetchMarkers.ts
+++ src/helperFunctions/fetchMarkers.ts
@@ -1,9 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 const fetchMarkers = async () => {
   try {
     const response = await axios.get(
-      `${process.env.REACT_APP_API_URL}api/markers`
+      `${process.env.REACT_APP_API_URL}api/markers`,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Security-Policy': 'upgrade-insecure-requests',
+        },
+      }
     );
     if (response.status === 200) {
       return response.data;
