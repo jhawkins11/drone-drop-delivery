51ab1324289880ced3d139221a274cb5e9d76ba3
jhawkins11 <josiah.c.hawkins@gmail.com>
2023-10-13 11:27:11 -0500

    delete unneccessary comment

--- src/helperFunctions/addMarker.ts
+++ src/helperFunctions/addMarker.ts
@@ -1,19 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const addMarker = async (marker) => {
   try {
-    // const response = await axios.post(
-    //   `${process.env.REACT_APP_API_URL}api/markers`,
-    //   marker
-    // );
-    // rewrite with headers
-    const response = await axios.post(`/api/api/markers`, marker);
+    const response = await axios.post(`/api/api/markers`, marker)
     if (response.status === 200) {
-      return response.data;
+      return response.data
     }
   } catch (err) {
-    throw new Error(err.message);
+    throw new Error(err.message)
   }
-};
+}
 
-export default addMarker;
+export default addMarker
